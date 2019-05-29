ab87c75e423051db1165d1e6252f18e9125434de
imranweb <imranweb97@gmail.com>
2019-05-29 14:11:38 +0530

    blog package test commit

--- packages/blog/src/pages/index.js
+++ packages/blog/src/pages/index.js
@@ -11,7 +11,7 @@ const BlogPage = ({ data }) => {
       <ItemCardWrapper>
         {edges && edges.map(edge => <ItemCard key={edge.node.id} data={edge.node} slug="post" />)}
       </ItemCardWrapper>
-      <a href={process.env.SHOP_URL}>Back to Shop</a>
+      <a href={process.env.SHOP_URL}>Go to Shop</a>
     </div>
   )
 }
