e0cd586bd85254db9ec0fec1993436b3b94343ad
imranweb <imranweb97@gmail.com>
2019-05-29 16:06:16 +0530

    fixed shop package

--- packages/shop/gatsby-config.js
+++ packages/shop/gatsby-config.js
@@ -1,5 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Learning Lerna through Monorepos with Rico',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-json',
+    'gatsby-plugin-react-next',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'data',
+        path: `${__dirname}/data/`,
+      },
+    },
+  ],
+}
+
 const activeEnv = process.env.ACTIVE_ENV | 'development'
 
 require('dotenv').config({
-  path: `.env.${activeEnv}`,
+path: `.env.${activeEnv}`,
 })
